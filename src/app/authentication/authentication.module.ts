1a39f825682c577cfb6d274cde03c438327fc818
Borja Arias Drake <borja@mobilejazz.com>
2022-04-02 10:48:51 +0200

    feat(auth): add login component template and form logic

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginFormComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  exports: [LoginFormComponent],
 })
 export class AuthenticationModule {}
--- src/app/authentication/components/login-form/login-form.component.ts
+++ src/app/authentication/components/login-form/login-form.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroupDirective, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.sass']
+  styleUrls: ['./login-form.component.sass'],
 })
 export class LoginFormComponent implements OnInit {
+  loginForm = this.formBuilder.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+  constructor(private formBuilder: FormBuilder) {}
 
-  constructor() { }
+  isSubmitted: boolean = false;
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit() {
+    this.isSubmitted = true;
+    if (this.loginForm.invalid) {
+      this.loginForm.reset();
+      return;
+    }
   }
 
+  isUsernameInvalid(): boolean {
+    return this.isFieldInvalid('username');
+  }
+
+  isUsernameRequiredError(): string | null {
+    return this.isFieldRequiredError('username');
+  }
+
+  isPasswordInvalid(): boolean {
+    return this.isFieldInvalid('username');
+  }
+
+  isPasswordRequiredError(): string | null {
+    return this.isFieldRequiredError('username');
+  }
+
+  private isFieldInvalid(key: string): boolean {
+    return this.isSubmitted && this.loginForm.controls[key].errors != null;
+  }
+
+  private isFieldRequiredError(key: string): string | null {
+    let errors = this.loginForm.controls[key].errors;
+
+    if (errors) {
+      return errors['required'];
+    } else {
+      return null;
+    }
+  }
 }
