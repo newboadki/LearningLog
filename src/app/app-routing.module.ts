0cb41cee4949791806ed8bc96b97cf20ab700838
Borja Arias Drake <borja@mobilejazz.com>
2022-04-02 06:30:39 +0200

    feat(auth): add login-form component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './authentication/components/login-form/login-form.component';
 
-const routes: Routes = [];
+export class RoutingPaths {
+  public static loginPath = 'login';
+}
+const routes: Routes = [
+  {
+    path: RoutingPaths.loginPath,
+    component: LoginFormComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [LoginFormComponent],
+  imports: [CommonModule],
 })
-export class AuthenticationModule { }
+export class AuthenticationModule {}
